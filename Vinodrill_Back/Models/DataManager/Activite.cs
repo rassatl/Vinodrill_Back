28a56c919fe5bed96e4722899d63212416ef9e80
cherifad <cherifad@D250-S09>
2023-03-06 15:28:59 +0100

    Création Model Activite

---
+++ Vinodrill_Back/Models/DataManager/Activite.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vinodrill_Back.Models.DataManager
+{
+    [Table("t_e_activite_act")]
+    public partial class Activite
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("act_id")]
+        public int IdActivite { get; set; }
+
+        [ForeignKey("fk_part_act")]
+        [Column("act_idpartenaire")]
+        public int IdPartenaire { get; set; }
+
+        [Column("act_libelle")]
+        [StringLength(255)]
+        public string LibelleActivite { get; set; }
+
+        [Column("act_description")]
+        public string DescriptionActivite { get; set; }
+
+        [Column("act_ruerdv")]
+        [StringLength(255)]
+        public string RueRdv { get; set; }
+
+        [Column("act_cprdv")]
+        [StringLength(5)]
+        public string CpRdv { get; set; }
+
+        [Column("act_villerdv")]
+        [StringLength(255)]
+        public string VilleRdv { get; set; }
+
+        [Column("act_horaireactivite")]
+        public TimeOnly HoraireActivite { get; set; }
+    }
+}
