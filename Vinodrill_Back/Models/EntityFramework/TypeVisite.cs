4f8673ddd739e7446d53c7e76b2135e96b60e476
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-06 17:30:26 +0100

    ajout model type visite

---
+++ Vinodrill_Back/Models/EntityFramework/TypeVisite.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_typevisite_tvst")]
+    public partial class TypeVisite
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("tvst_id")]
+        [Required]
+        public int IdTypeVisite { get; set; }
+
+        [Column("tvst_libelle")]
+        [StringLength(250, ErrorMessage = "the libelle type visite must be 255 maximum")]
+        [Required]
+        public string LibelleTypeVisite { get; set; }
+    }
+}
