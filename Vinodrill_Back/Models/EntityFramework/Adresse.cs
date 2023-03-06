7cc991646847f48c13f1ab7d3dcdc95fb6df0a7a
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-06 15:27:51 +0100

    Création Class Model Adresse et Client

---
+++ Vinodrill_Back/Models/EntityFramework/Adresse.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_adresse_adr")]
+    public partial class Adresse
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("adr_id")]
+        [Required]
+        public int IdAdresse { get; set; }
+
+        [Column("adr_libelle")]
+        [StringLength(250, ErrorMessage = "the length of the caption must be 250 maximum")]
+        [Required]
+        public string LibelleAdresse { get; set; }
+
+        [Column("adr_rueadresse")]
+        [StringLength(250, ErrorMessage = "the length of the address must be 250 maximum")]
+        [Required]
+        public string RueAdresse { get; set; }
+
+        [Column("adr_ville")]
+        [StringLength(250, ErrorMessage = "the length of the city must be 250 maximum")]
+        [Required]
+        public string VilleAdresse { get; set; }
+
+        [Column("adr_cp")]
+        [RegularExpression(@"^[0-9]{5}$", ErrorMessage = "postcode length must be 5 maximum")]
+        [Required]
+        public string CodePostalAdresse { get; set; }
+
+        [Column("adr_pays")]
+        [StringLength(5, ErrorMessage = "country length should be 25 maximum")]
+        [Required]
+        public string PaysAdresse { get; set; }
+        
+        [InverseProperty("ClientAdresse")]
+        public virtual ICollection<Client>? ClientNavigation { get; set; } = null!;
+
+    }
+}
