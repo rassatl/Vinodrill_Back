2d1ba1d5228c7536234f1294577d82e1a38b0b20
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-06 16:09:34 +0100

    Modif Models Adresse et Client

--- Vinodrill_Back/Models/EntityFramework/Client.cs
+++ Vinodrill_Back/Models/EntityFramework/Client.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace Vinodrill_Back.Models.EntityFramework
 {
     [Table("t_e_client_clt")]
-    public partial class Adresse
+    public partial class Client
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -12,28 +12,45 @@ namespace Vinodrill_Back.Models.EntityFramework
         [Required]
         public int IdClient { get; set; }
 
-        [Column("clt_avis")]
+        [ForeignKey("fk_avis_clt")]
+        [Column("clt_idavis")]
         [Required]
-        public int IdAvisClient { get; set; }
+        public int? IdAvisClient { get; set; } = null;
 
-        [Column("adr_rueadresse")]
+        [ForeignKey("fk_cb_clt")]
+        [Column("clt_idcb")]
         [Required]
         public int IdCbClient { get; set; }
 
-        [Column("adr_ville")]
-        [StringLength(250, ErrorMessage = "the length of the city must be 250 maximum")]
+        [Column("clt_nom")]
+        [StringLength(255, ErrorMessage = "the client name must be 255 maximum")]
         [Required]
         public string NomClient { get; set; }
 
-        [Column("adr_cp")]
-        [RegularExpression(@"^[0-9]{5}$", ErrorMessage = "postcode length must be 5 maximum")]
+        [Column("clt_prenom")]
+        [StringLength(255, ErrorMessage = "the client first name must be 255 maximum")]
         [Required]
-        public string CodePostal { get; set; }
+        public string PrenomClient { get; set; }
 
-        [Column("adr_pays")]
-        [StringLength(5, ErrorMessage = "country length should be 25 maximum")]
+        [Column("clt_datenaissance", TypeName = "date")]
         [Required]
-        public string Pays { get; set; }
+        public DateTime DateNaissanceClient { get; set; }
+
+        [Column("clt_sexe")]
+        [RegularExpression(@"^[0-9]{1}$", ErrorMessage = "sexe length must be 1 maximum")]
+        [Required]
+        public string SexeClient { get; set; }
+
+        [Column("clt_motdepasse")]
+        [StringLength(255, ErrorMessage = "motdepasse length must be 255 maximum")]
+        [Required]
+        public string MotDePasseClient { get; set; }
+
+        [Column("clt_email")]
+        [EmailAddress]
+        [StringLength(255, ErrorMessage = "email length must be 255 maximum")]
+        [Required]
+        public string EmailClient { get; set; }
 
         [InverseProperty("ClientAdresse")]
         public virtual ICollection<Client>? ClientNavigation { get; set; } = null!;
