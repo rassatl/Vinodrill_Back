1aab393a9feb7d091d04af5a0e2e3c0e4c217f6b
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-06 17:22:59 +0100

    ajout model destination

---
+++ Vinodrill_Back/Models/EntityFramework/Destination.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_destination_dst")]
+    public partial class Destination
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("dst_id")]
+        [Required]
+        public int IdDestination { get; set; }
+
+        [Column("dst_libelle")]
+        [Required]
+        [StringLength(255, ErrorMessage = "the length of the destination must be 255 maximum")]
+        public string LibelleDestination { get; set; }
+
+        [Column("dst_description")]
+        [Required]
+        public string DescriptionDestination { get; set; }
+    }
+}
