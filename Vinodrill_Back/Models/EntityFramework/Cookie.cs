2f81d158bbc7e872fb581deaa8df9e3c4972cea1
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-06 17:04:28 +0100

    Ajout Model cookie

---
+++ Vinodrill_Back/Models/EntityFramework/Cookie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_cookie_cke")]
+    public partial class Cookie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("cke_id")]
+        [Required]
+        public int IdCookie { get; set; }
+
+        [Column("cke_libelle")]
+        [StringLength(250, ErrorMessage = "the cookie label length must be 250 maximum")]
+        [Required]
+        public string LibelleCookie { get; set; }
+
+        [InverseProperty("ClientAdresse")]
+        public virtual ICollection<Client>? ClientNavigation { get; set; } = null!;
+
+    }
+}
