b5d607f066613441cd59d3316dfe9d80b3dc45dc
omerovia <ahmed.omerovic@etu.univ-smb.fr>
2023-03-07 13:38:57 +0100

    + etape

---
+++ Vinodrill_Back/Models/EntityFramework/Etape.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_etape_etp")]
+    public partial class Etape
+    {
+
+        [Key]
+        [ForeignKey("fk_etp_sje")]
+        [Column("sjr_id", Order = 0)]
+        public int IdSejour { get; set; }
+
+        [Key]
+        [ForeignKey("fk_etp_hbg")]
+        [Column("hbg_id", Order = 1)]
+        public int IdHebergement{ get; set; }
+
+        [Key]
+        [Column("etp_id")]
+        public int IdEtape { get; set; }
+
+        [Column("etp_titre")]
+        [StringLength(255, ErrorMessage = " titre lenght must be 255 maximum")]
+        [Required]
+        public string TitreEtape { get; set; }
+
+        [Column("etp_description")]
+        [Required]
+        public string DescriptionEtape { get; set; }
+
+        [Column("etp_photo")]
+        [Required]
+        public string PhotoEtape { get; set; }
+
+        [Column("etp_url")]
+        [StringLength(255, ErrorMessage = " URL lenght must be 255 maximum")]
+        [Required]
+        public string UrlEtape { get; set; }
+
+        [Column("etp_video")]
+        [StringLength(255, ErrorMessage = " Video lenght must be 255 maximum")]
+        [Required]
+        public string VideoEtape { get; set; }
+    }
+}
