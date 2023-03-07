653354c08a61bafff3c50fe1336e2606cfa56c36
omerovia <ahmed.omerovic@etu.univ-smb.fr>
2023-03-07 16:50:17 +0100

    + Fais Partie de

--- Vinodrill_Back/Models/EntityFramework/FaitPartieDe.cs
+++ Vinodrill_Back/Models/EntityFramework/FaitPartieDe.cs
@@ -1,6 +1,21 @@
-﻿namespace Vinodrill_Back.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
 {
-    public class FaitPartieDe
+    [Table("t_j_faitpartiede_fpd")]
+    public partial class FaitPartieDe
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ForeignKey("fk_vst_fpd")]
+        [Column("vst_id", Order = 0)]
+        public int IdVisite { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ForeignKey("fk_etp_fpd")]
+        [Column("etp_id", Order = 1)]
+        public int IdEtape { get; set; }
     }
 }
