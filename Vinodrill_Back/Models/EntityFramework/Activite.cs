f4d3d998304cf59fd3b7d2b54aa95c58c6a79b83
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-07 14:59:22 +0100

    correction erreurs activite

--- Vinodrill_Back/Models/EntityFramework/Activite.cs
+++ Vinodrill_Back/Models/EntityFramework/Activite.cs
@@ -16,22 +16,22 @@ namespace Vinodrill_Back.Models.EntityFramework
         public int IdPartenaire { get; set; }
 
         [Column("act_libelle")]
-        [StringLength(255)]
+        [StringLength(255, ErrorMessage = "libelle must be 255 maximum")]
         public string LibelleActivite { get; set; }
 
         [Column("act_description")]
         public string DescriptionActivite { get; set; }
 
         [Column("act_ruerdv")]
-        [StringLength(255)]
+        [StringLength(255, ErrorMessage = "rue must be 255 maximum")]
         public string RueRdv { get; set; }
 
         [Column("act_cprdv")]
-        [StringLength(5)]
+        [StringLength(5, ErrorMessage = "Cp must be 5 maximum")]
         public string CpRdv { get; set; }
 
         [Column("act_villerdv")]
-        [StringLength(255)]
+        [StringLength(255, ErrorMessage = "ville must be 255 maximum")]
         public string VilleRdv { get; set; }
 
         [Column("act_horaire")]
