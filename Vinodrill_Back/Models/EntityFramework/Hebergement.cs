2ff3becb966980983288691553fb5990f12df433
omerovia <ahmed.omerovic@etu.univ-smb.fr>
2023-03-07 13:47:32 +0100

    + hebergement

---
+++ Vinodrill_Back/Models/EntityFramework/Hebergement.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vinodrill_Back.Models.EntityFramework
+{
+    [Table("t_e_hebergement_hbg")]
+    public partial class Hebergement
+    {
+
+        [ForeignKey("fk_hbg_prt")]
+        [Column("prt_id")]
+        public int IdPartenaire { get; set; }
+
+        [Key]
+        [Column("hbg_id")]
+        public int IdHebergement { get; set; }
+
+        [Column("hbg_libelle")]
+        [StringLength(255, ErrorMessage = " libelle lenght must be 255 maximum")]
+        [Required]
+        public string LibelleHebergement { get; set; }
+
+        [Column("hbg_description")]
+        [Required]
+        public string DescriptionHebergement { get; set; }
+
+        [Column("hbg_nbchambre")]
+        [Required]
+        public int NbChambre { get; set; }
+
+        [Column("hbg_horaire")]
+        [StringLength(255, ErrorMessage = " horaire lenght must be 255 maximum")]
+        [Required]
+        public TimeOnly HoraireHebergement { get; set; }
+
+
+    }
+}
