84ebf6ccd82717b15f1dcf94b97ed9155dd14e90
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-07 14:55:54 +0100

    modif imageAvi

--- Vinodrill_Back/Models/EntityFramework/ImageAvi.cs
+++ Vinodrill_Back/Models/EntityFramework/ImageAvi.cs
@@ -6,10 +6,14 @@ namespace Vinodrill_Back.Models.EntityFramework
     [Table("t_j_imageavi_imga")]
     public partial class ImageAvi
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [ForeignKey("fk_imga_avis")]
         [Column("avis_id", Order = 0)]
         public int IdAvis { get; set; }
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [ForeignKey("fk_imga_img")]
         [Column("img_id", Order = 1)]
         public int IdImage { get; set; }
