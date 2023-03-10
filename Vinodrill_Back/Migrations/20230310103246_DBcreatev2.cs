e6162b4b1cde917ee5c4b84a9596622568aef5bf
cherifad <cherifad@A275-S24>
2023-03-10 11:41:43 +0100

    DB Create migrations

---
+++ Vinodrill_Back/Migrations/20230310103246_DBcreatev2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vinodrill_Back.Migrations
+{
+    public partial class DBcreatev2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "uq_prt_email",
+                table: "t_e_partenaire_prt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "uq_prt_email",
+                table: "t_e_partenaire_prt",
+                column: "prt_email",
+                unique: true);
+        }
+    }
+}
