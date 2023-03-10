7a36f98664c8956560674a9c97238b220694fb4d
Omerovic <ahmed.omerovic@etu.univ-savoie.fr>
2023-03-10 11:23:21 +0100

    Partenaires Controller Bon

--- Vinodrill_Back/Controllers/PartenairesController.cs
+++ Vinodrill_Back/Controllers/PartenairesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Vinodrill_Back.Models.EntityFramework;
+using Vinodrill_Back.Models.Repository;
 
 namespace Vinodrill_Back.Controllers
 {
@@ -13,95 +14,97 @@ namespace Vinodrill_Back.Controllers
     [ApiController]
     public class PartenairesController : ControllerBase
     {
-        private readonly VinodrillDBContext _context;
+        private readonly IDataRepository<Partenaire> dataRepository;
 
-        public PartenairesController(VinodrillDBContext context)
+        public PartenairesController(IDataRepository<Partenaire> dataRepo)
         {
-            _context = context;
+            dataRepository = dataRepo;
         }
 
         // GET: api/Partenaires
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Partenaire>>> GetPartenaire()
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Partenaire>))]
+        public async Task<ActionResult<IEnumerable<Partenaire>>> GetPartenaires()
         {
-            return await _context.Partenaire.ToListAsync();
+            return await dataRepository.GetAll();
         }
 
-        // GET: api/Partenaires/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Partenaire>> GetPartenaire(int id)
+        // GET: api/Partenaire/GetPartenaireById/1
+        [HttpGet("GetPartenaireById/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Partenaire))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Partenaire>> GetPartenaireById(int id)
         {
-            var partenaire = await _context.Partenaire.FindAsync(id);
+            var Activite = await dataRepository.GetById(id);
 
-            if (partenaire == null)
+            if (Activite == null)
             {
                 return NotFound();
             }
 
-            return partenaire;
+            return Activite;
+
+            /*return StatusCode(StatusCodes.Status405MethodNotAllowed);*/
         }
 
-        // PUT: api/Partenaires/5
+        ///PUT: api/Partenaire/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutPartenaire(int id, Partenaire partenaire)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutPartenaires(int id, Partenaire Partenaire)
         {
-            if (id != partenaire.IdPartenaire)
+
+            if (id != Partenaire.IdPartenaire)
             {
                 return BadRequest();
             }
 
-            _context.Entry(partenaire).State = EntityState.Modified;
-
-            try
+            var userToUpdate = await dataRepository.GetById(id);
+            if (userToUpdate == null)
             {
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!PartenaireExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                await dataRepository.Update(userToUpdate.Value, Partenaire);
+                return NoContent();
             }
-
-            return NoContent();
         }
+
 
         // POST: api/Partenaires
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Partenaire>> PostPartenaire(Partenaire partenaire)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Partenaire))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Partenaire>> PostPartenaire(Partenaire Partenaire)
         {
-            _context.Partenaire.Add(partenaire);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await dataRepository.Add(Partenaire);
 
-            return CreatedAtAction("GetPartenaire", new { id = partenaire.IdPartenaire }, partenaire);
+            return CreatedAtAction("GetPartenaireById", new { id = Partenaire.IdPartenaire }, Partenaire);
         }
 
         // DELETE: api/Partenaires/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeletePartenaire(int id)
         {
-            var partenaire = await _context.Partenaire.FindAsync(id);
-            if (partenaire == null)
+            var Partenaire = await dataRepository.GetById(id);
+            if (Partenaire == null)
             {
                 return NotFound();
             }
 
-            _context.Partenaire.Remove(partenaire);
-            await _context.SaveChangesAsync();
+            await dataRepository.Delete(Partenaire.Value);
 
             return NoContent();
-        }
-
-        private bool PartenaireExists(int id)
-        {
-            return _context.Partenaire.Any(e => e.IdPartenaire == id);
         }
     }
 }
