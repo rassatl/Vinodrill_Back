d9f20479c951d56b6437a166ab82817e680969f3
cherifad <95074031+Nelda74@users.noreply.github.com>
2023-03-12 14:37:45 +0100

    Manager to interact with 'sejour' table

--- Vinodrill_Back/Models/DataManager/SejourManager.cs
+++ Vinodrill_Back/Models/DataManager/SejourManager.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Vinodrill_Back.Models.DataManager
 {
-    public class SejourManager : IDataRepository<Sejour>
+    public class SejourManager : ISejourRepository
     {
         readonly VinodrillDBContext? dbContext;
 
@@ -35,15 +35,77 @@ namespace Vinodrill_Back.Models.DataManager
         public async Task<ActionResult<IEnumerable<Sejour>>> GetAll()
         {
             // get theme and destination
-            return await dbContext.Sejours
-                .Include(s => s.ThemeSejourNavigation)
-                .Include(s => s.DestinationSejourNavigation)
-                .ToListAsync();
+            return await dbContext.Sejours.ToListAsync();
         }
 
         public async Task<ActionResult<Sejour>> GetById(int id)
         {
             return await dbContext.Sejours.FirstOrDefaultAsync(a => a.IdSejour == id);
+        }
+
+        public async Task<ActionResult<IEnumerable<Sejour>>> GetAllFromSpecificDestination(string idsDestination)
+        {
+            try {
+                var ids = idsDestination.Split(',').Select(int.Parse).ToArray();
+                return await dbContext.Sejours
+                    .Where(s => ids.Contains(s.IdDestination))
+                    .Include(s => s.ThemeSejourNavigation)
+                    .Include(s => s.DestinationSejourNavigation)
+                    .ToListAsync();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        public async Task<ActionResult<IEnumerable<Sejour>>> GetAllFromSpecificTheme(string idsTheme)
+        {
+            try {
+                var ids = idsTheme.Split(',').Select(int.Parse).ToArray();
+                return await dbContext.Sejours
+                    .Where(s => ids.Contains(s.IdTheme))
+                    .ToListAsync();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        public async Task<ActionResult<IEnumerable<Sejour>>> GetAllFromSpecificCatParticipant(string idsCatParticipant)
+        {
+            try {
+                // get all id from query parameter
+                var ids = idsCatParticipant.Split(',').Select(int.Parse).ToArray();
+
+                // get all id sejour from Participe table
+                var catParticipant = await dbContext.Participe
+                    .Where(c => ids.Contains(c.IdCategorieParticipant))
+                    .ToListAsync();
+
+                // get all id sejour from the previous query
+                var idsSejour = catParticipant.Select(c => c.IdSejour).ToArray();
+
+                // get all sejour with the id sejour from the previous query
+                return await dbContext.Sejours
+                    .Where(s => idsSejour.Contains(s.IdSejour))
+                    .ToListAsync();
+
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        public Task<ActionResult<IEnumerable<Sejour>>> GetAllExceptSpecificSejour(int idSejour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<Sejour>>> GetAllSetLimit(int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<Sejour>>> GetMultipleSejour(string idsSejour)
+        {
+            throw new NotImplementedException();
         }
     }
 }
