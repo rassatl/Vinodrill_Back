356c1e3cb1942a597296db68bbfe4d49d8c4d200
cherifad <95074031+Nelda74@users.noreply.github.com>
2023-03-12 14:40:20 +0100

    JWT service configuration

--- Vinodrill_Back/Program.cs
+++ Vinodrill_Back/Program.cs
@@ -2,6 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Vinodrill_Back.Models.DataManager;
 using Vinodrill_Back.Models.EntityFramework;
 using Vinodrill_Back.Models.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Vinodrill_Back.Auth;
 
 namespace Vinodrill_Back
 {
@@ -17,16 +22,48 @@ namespace Vinodrill_Back
                 {
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
-                
+
+            // For Entity Framework
+            builder.Services.AddDbContext<VinodrillDBContext>(Options => Options.UseNpgsql(builder.Configuration.GetConnectionString("VinoDrillDbContext")));
+            builder.Services.AddDbContext<AuthDbContext>(Options => Options.UseNpgsql(builder.Configuration.GetConnectionString("VinoDrillDbContext")));
+
+            // For Identity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Adding Authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                };
+            });
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddDbContext<VinodrillDBContext>(Options => Options.UseNpgsql(builder.Configuration.GetConnectionString("VinoDrillDbContextRemote")));
             builder.Services.AddScoped<IDataRepository<Activite>, ActiviteManager>();
             builder.Services.AddScoped<IDataRepository<Adresse>, AdresseManager>();
             builder.Services.AddScoped<IAvisRepository, AviManager>();
+            builder.Services.AddScoped<ISejourRepository, SejourManager>();
 
 
             var app = builder.Build();
@@ -38,10 +75,20 @@ namespace Vinodrill_Back
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(options =>
+            {
+                options.AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials();
+            });
+
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // Authentication & Authorization
+            app.UseAuthentication();
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
