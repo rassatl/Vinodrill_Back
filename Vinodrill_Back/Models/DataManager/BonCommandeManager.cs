9da1343922eb24762b366f942aebfe39f022ddfe
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-03-13 17:31:39 +0100

    ajout client et boncommande manager

---
+++ Vinodrill_Back/Models/DataManager/BonCommandeManager.cs
@@ -0,0 +1,50 @@
+﻿using Vinodrill_Back.Models.EntityFramework;
+using Vinodrill_Back.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vinodrill_Back.Models.DataManager
+{
+    public class BonCommandeManager : IDataRepository<BonCommande>
+    {
+        readonly VinodrillDBContext? dbContext;
+
+        public BonCommandeManager() { }
+
+        public BonCommandeManager(VinodrillDBContext context) { dbContext = context; }
+
+        public async Task Add(BonCommande entity)
+        {
+            await dbContext.BonCommandes.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(BonCommande entityToUpdate, BonCommande entity)
+        {
+            dbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.IdBonCommande = entity.IdBonCommande;
+            entityToUpdate.RefCommande = entity.RefCommande;
+            entityToUpdate.CodeBonCommande = entity.CodeBonCommande;
+            entityToUpdate.DateValidite = entity.DateValidite;
+            entityToUpdate.EstValide = entity.EstValide;
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(BonCommande entity)
+        {
+            dbContext.BonCommandes.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<BonCommande>>> GetAll()
+        {
+            return await dbContext.BonCommandes.ToListAsync();
+        }
+
+        public async Task<ActionResult<BonCommande>> GetById(int id)
+        {
+            return await dbContext.BonCommandes.FirstOrDefaultAsync(a => a.IdBonCommande == id);
+        }
+    }
+}
---
+++ Vinodrill_Back/Models/DataManager/ClientManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vinodrill_Back.Models.EntityFramework;
+using Vinodrill_Back.Models.Repository;
+
+namespace Vinodrill_Back.Models.DataManager
+{
+    public class ClientManager : IDataRepository<Client>
+    {
+        readonly VinodrillDBContext? dbContext;
+
+        public ClientManager() { }
+
+        public ClientManager(VinodrillDBContext context) { dbContext = context; }
+
+        public async Task Add(Client entity)
+        {
+            await dbContext.Clients.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Client entityToUpdate, Client entity)
+        {
+            dbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.IdClient = entity.IdClient;
+            entityToUpdate.IdCbClient = entity.IdCbClient;
+            entityToUpdate.IdAvisClient = entity.IdAvisClient;
+            entityToUpdate.EmailClient = entity.EmailClient;
+            entityToUpdate.SexeClient = entity.SexeClient;
+            entityToUpdate.NomClient = entity.NomClient;
+            entityToUpdate.PrenomClient = entity.PrenomClient;
+            entityToUpdate.MotDePasseClient= entity.MotDePasseClient;
+            entityToUpdate.DateNaissanceClient = entity.DateNaissanceClient;
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(Client entity)
+        {
+            dbContext.Clients.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<Client>>> GetAll()
+        {
+            return await dbContext.Clients.ToListAsync();
+        }
+
+        public async Task<ActionResult<Client>> GetById(int id)
+        {
+            return await dbContext.Clients.FirstOrDefaultAsync(a => a.IdClient == id);
+        }
+    }
+}
