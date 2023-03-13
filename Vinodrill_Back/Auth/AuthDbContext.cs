874223e4c877a145e9f651acb88562613c01c09e
cherifad <cherifad@A275-S24>
2023-03-13 17:02:28 +0100

    Add custom user table

--- Vinodrill_Back/Auth/AuthDbContext.cs
+++ Vinodrill_Back/Auth/AuthDbContext.cs
@@ -1,17 +1,60 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace Vinodrill_Back.Auth
 {
-    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    public class AuthDbContext : IdentityDbContext<User>
     {
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser>(b =>
+            {
+                b
+                   .HasIndex(e => e.Email)
+                   .IsUnique()
+                   .HasDatabaseName("uq_clt_email");
+
+                b.ToTable("t_e_user_usr");
+
+                b.Ignore(c => c.PhoneNumberConfirmed);
+            });
+
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("t_e_userclaim_ucl");
+            });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.ToTable("t_e_userlogin_ulg");
+            });
+
+            modelBuilder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.ToTable("t_e_usertoken_uto");
+            });
+
+            modelBuilder.Entity<IdentityRole>(b =>
+            {
+                b.ToTable("t_e_role_rle");
+            });
+
+            modelBuilder.Entity<IdentityRoleClaim<string>>(b =>
+            {
+                b.ToTable("t_e_roleclaim_rcl");
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.ToTable("t_e_userrole_url");
+            });
         }
     }
 }
