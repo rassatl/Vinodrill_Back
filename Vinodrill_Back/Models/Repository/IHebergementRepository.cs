59bc866f41b95fdd6ce175bea11d105072b42a56
reydetb <baptiste.reydet@etu.univ-smb.fr>
2023-03-14 09:15:16 +0100

    JSP

---
+++ Vinodrill_Back/Models/Repository/IHebergementRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vinodrill_Back.Models.EntityFramework;
+
+namespace Vinodrill_Back.Models.Repository
+{
+    public interface IHebergementRepository : IDataRepository<Hebergement>
+    {
+        Task<ActionResult<Hebergement>> GetAllSpecificWithEtapeAndHotel(string idsEtape = "notImplemented", int idHotel = 0);
+
+    }
+}
