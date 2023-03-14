ecb49d4c4a39e2cb96acb1a1eee76def75614c2a
Omerovic <ahmed.omerovic@etu.univ-savoie.fr>
2023-03-14 09:11:10 +0100

    Hebergement + BonCommande(a ajouter des fonction)

---
+++ Vinodrill_Back/Controllers/BonCommandeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vinodrill_Back.Models.EntityFramework;
+
+namespace Vinodrill_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BonCommandeController : ControllerBase  // des bails a ajouter
+    {
+        private readonly VinodrillDBContext _context;
+
+        public BonCommandeController(VinodrillDBContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/BonCommande/5
+        [HttpGet("{refcommande}")]
+        public async Task<ActionResult<BonCommande>> GetBonCommande(int refcommande)
+        {
+            var bonCommande = await _context.BonCommandes.FindAsync(refcommande);
+
+            if (bonCommande == null)
+            {
+                return NotFound();
+            }
+
+            return bonCommande;
+        }
+    }
+}
