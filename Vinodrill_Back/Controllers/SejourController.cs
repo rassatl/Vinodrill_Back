9b09f0f770afca9141a34dc1bc806f8a29564c08
cherifad <cherifad@A275-S24>
2023-03-14 10:37:24 +0100

    Add Post And Put

--- Vinodrill_Back/Controllers/SejourController.cs
+++ Vinodrill_Back/Controllers/SejourController.cs
@@ -49,63 +49,63 @@ namespace Vinodrill_Back.Controllers
             // return StatusCode(StatusCodes.Status405MethodNotAllowed);
         }
 
-        // //PUT: api/Avis/5
-        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        // [HttpPut("{id}")]
-        // [ProducesResponseType(StatusCodes.Status204NoContent)]
-        // [ProducesResponseType(StatusCodes.Status404NotFound)]
-        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        // public async Task<IActionResult> PutAdresse(int id, Avis avi)
-        // {
+        //PUT: api/Sejour/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutAdresse(int id, Sejour sejour)
+        {
 
-        //     if (id != avi.IdAvis)
-        //     {
-        //         return BadRequest();
-        //     }
+            if (id != sejour.IdSejour)
+            {
+                return BadRequest();
+            }
 
-        //     var userToUpdate = await dataRepository.GetById(id);
-        //     if (userToUpdate == null)
-        //     {
-        //         return NotFound();
-        //     }
-        //     else
-        //     {
-        //         await dataRepository.Update(userToUpdate.Value, avi);
-        //         return NoContent();
-        //     }
-        // }
+            var userToUpdate = await dataRepository.GetById(id);
+            if (userToUpdate == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                await dataRepository.Update(userToUpdate.Value, sejour);
+                return NoContent();
+            }
+        }
 
-        // // POST: api/Avis
-        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        // [HttpPost]
-        // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Avis))]
-        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        // public async Task<ActionResult<Avis>> PostAdresse(Avis avi)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return BadRequest(ModelState);
-        //     }
-        //     await dataRepository.Add(avi);
+        // POST: api/Avis
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Avis))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Avis>> PostAdresse(Avis avi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await dataRepository.Add(avi);
 
-        //     return CreatedAtAction("GetAvisById", new { id = avi.IdAvis }, avi);
-        // }
+            return CreatedAtAction("GetAvisById", new { id = avi.IdAvis }, avi);
+        }
 
-        // // DELETE: api/Avis/5
-        // [HttpDelete("{id}")]
-        // [ProducesResponseType(StatusCodes.Status204NoContent)]
-        // [ProducesResponseType(StatusCodes.Status404NotFound)]
-        // public async Task<IActionResult> DeleteAvis(int id)
-        // {
-        //     var avi = await dataRepository.GetById(id);
-        //     if (avi == null)
-        //     {
-        //         return NotFound();
-        //     }            
+        // DELETE: api/Avis/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteAvis(int id)
+        {
+            var avi = await dataRepository.GetById(id);
+            if (avi == null)
+            {
+                return NotFound();
+            }
 
-        //     await dataRepository.Delete(avi.Value);
+            await dataRepository.Delete(avi.Value);
 
-        //     return NoContent();
-        // }
+            return NoContent();
+        }
     }
 }
