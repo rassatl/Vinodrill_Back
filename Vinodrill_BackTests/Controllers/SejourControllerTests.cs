27f275bbe34b8e7c924116d67a51016d22d7c472
Omerovic <ahmed.omerovic@etu.univ-savoie.fr>
2023-03-14 10:49:26 +0100

    Tests

---
+++ Vinodrill_BackTests/Controllers/SejourControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vinodrill_Back.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinodrill_Back.Controllers.Tests
+{
+    [TestClass()]
+    public class SejourControllerTests
+    {
+        [TestMethod()]
+        public void DeleteAvisTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
