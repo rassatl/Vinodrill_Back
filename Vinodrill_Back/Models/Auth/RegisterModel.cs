23e67d9d336da7ce1c0428b110e6d3670dbe75f1
reydetb <baptiste.reydet@etu.univ-smb.fr>
2023-03-31 11:50:29 +0200

    rregister en francais

--- Vinodrill_Back/Models/Auth/RegisterModel.cs
+++ Vinodrill_Back/Models/Auth/RegisterModel.cs
@@ -5,27 +5,27 @@ namespace Vinodrill_Back.Models.Auth
     public class RegisterModel
     {
         [EmailAddress]
-        [Required(ErrorMessage = "Email is required")]
+        [Required(ErrorMessage = "Veuillez renseigner un e-mail")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Veuillez renseigner un mot de passe")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        [Required(ErrorMessage = "Veuillez renseigner une confirmation de mot de passe")]
+        [Compare("Password", ErrorMessage = "Votre mot de passe et sa confirmation ne correspondent pas")]
         public string ConfirmPassword { get; set; }
 
-        [Required(ErrorMessage = "First Name is required")]
+        [Required(ErrorMessage = "Veuillez renseigner un prénom")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last Name is required")]
+        [Required(ErrorMessage = "Veuillez renseigner un nom")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Date of Birth is required")]
+        [Required(ErrorMessage = "Veuillez renseigner une date de naissance")]
         public DateTime DateOfBirth { get; set; }
 
-        [Required(ErrorMessage = "Gender is required")]
-        [StringLength(1, ErrorMessage = "Gender can be either M or F")]
+        [Required(ErrorMessage = "Veuillez renseigner un genre")]
+        [StringLength(1, ErrorMessage = "Votre genre doit être M ou F")]
         public string Gender { get; set; }
     }
 }
